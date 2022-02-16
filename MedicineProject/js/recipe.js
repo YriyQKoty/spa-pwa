6960f8dccab9acd8c59f8d2d9361881376e4ad8f
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2022-02-17 00:07:11 +0200

    Ionic SPA

--- MedicineProject/js/recipe.js
+++ MedicineProject/js/recipe.js
@@ -1,3 +1,93 @@
+let editType=''
+let recipeName =''
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var rowText;
+    var content = document.getElementById('list');
+    const recipes = getRecipes()
+ 
+    for (let row of recipes) {
+       rowText = document.createElement('ion-item');
+       rowText.innerHTML = `<ion-label>${row.name}</ion-label>
+       <ion-button class="edit" color="primary" size="medium" rowid="${row.name}">
+       <ion-icon name="create"></ion-icon>   
+        </ion-button>
+        <ion-button class="delete" color="danger" size="medium" rowid="${row.name}">
+         <ion-icon name="trash"></ion-icon>   
+        </ion-button> `;
+        content.appendChild(rowText);
+    }
+
+    document.querySelector('#main').style.display=""
+    document.querySelector('#edit').style.display="none"
+
+    document.querySelectorAll('.edit').forEach(input => input.addEventListener('click', ({target}) => {
+        editType = "edit"
+        recipeName = target.getAttribute('rowid')
+        let recipe = recipes.find((r) => r.name == recipeName)
+
+        document.getElementById('name').value = recipe.name
+        document.getElementById('quantity').value = recipe.quantity
+        document.getElementById('desc').value = recipe.desc
+        document.querySelector('#main').style.display = "none"
+        document.querySelector('#edit').style.display = ""
+    }));
+
+
+    document.querySelectorAll('.delete').forEach(input => input.addEventListener('click', ({target}) => {
+    
+        recipeName = target.getAttribute('rowid')
+        saveRecipes(recipes.filter((r) => r.name != recipeName))
+
+        location.reload()
+    }));
+
+    document.querySelector('#save').addEventListener('click', () => {
+        if(editType == "add") {
+            recipes.push({
+                name: document.getElementById('name').value,
+                quantity: document.getElementById('quantity').value,
+                desc: document.getElementById('desc').value
+            });
+        }
+        else {
+            let recipe = recipes.find((r) => r.name == recipeName)
+            recipe.name = document.getElementById("name").value
+            recipe.quantity = document.getElementById('quantity').value
+            recipe.desc =  document.getElementById('desc').value
+        }
+
+        saveRecipes(recipes)
+        location.reload();
+
+    });
+
+    document.querySelector('#add').addEventListener('click', () => {
+
+        document.querySelector('#main').style.display="none"
+        document.querySelector('#edit').style.display=""
+        document.getElementById('name').value=""
+        document.getElementById('quantity').value=""
+        document.getElementById('desc').value=""
+        editType="add"
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $('document').ready(function() {
    var rowText;
    var content = $('#recipes');
